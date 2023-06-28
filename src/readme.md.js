d85675cead5fd41b450cf881bc791b56cc43cfeb
Ashley Silva <silvashleya@gmail.com>
2023-06-28 10:13:58 -0400

    add readme.js & create scr folder

---
+++ src/readme.md.js
@@ -0,0 +1,10 @@
+// [✓] Step 0: Understand the problem CLEARLY
+// Problem: Register a user after all the inputs are OK!
+// [✓] be sure that no field are empty
+// [✓] be sure that the passwords match
+
+// STEP 1: DESIGN my algo (step -by- step to solve the prob.)
+    // [✓] a: write pseudo-code
+    // b: write real code (often starting with end-state + functions)
+    // c: test a small thing works, rinse and repeat
+    
